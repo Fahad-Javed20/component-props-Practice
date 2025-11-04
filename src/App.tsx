3ceccf321c791bfa54fc94b816744f86cf957b8f
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-05 01:18:32 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-
-import './App.css'
-import CardComponent from './components/CardComponent'
-
+import "./App.css";
+import CardComponent from "./components/CardComponent";
 
 function App() {
-
- 
   return (
     <>
-    <CardComponent hobbies={["Book Reading", "Content Creation", "Story Telling", "website development"]}/>
+      <CardComponent
+        hobbies={[
+          "Book Reading",
+          "Content Creation",
+          "Story Telling",
+          "website development",
+        ]}
+      />
     </>
-  )
-  
-  };
+  );
+}
 
-
-export default App
+export default App;
